1e85203735ed9f8976689ffde087a910cfcabe65
Lance Hebert <LSUHEBERT@gmail.com>
2021-12-04 20:38:45 -0800

    commit tree d1a590842f45e9f86b95da296394081279a47416

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+const body = document.querySelector("body");
+
+ submitData(userName,userEmail)
+
+function submitData  (userName,userEmail){
+    const infoObj = {
+        name:userName,
+        email:userEmail,
+    }
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+        body: JSON.stringify(infoObj),
+      
+        })
+        .then(resp => resp.json())
+        .then(users => {
+            console.log(users);
+            console.log(body);
+            body.append(users.id);
+
+        })
+        .catch(error => {
+            console.log(error.message)
+            body.append(error.message)
+        });
+}
+
+
+
